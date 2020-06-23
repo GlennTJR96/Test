a8a0d0e6c0b034a9494e7df1abe348950459df9a
GlennTan <e0310963@u.nus.edu>
2020-06-23 12:14:49 +0800

    3rd comit

---
+++ client/build/precache-manifest.d0ef327510eecf6f73df7af84b2d8a7a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6dea2ef7ab36b61efa68773bbc3a6ad",
+    "url": "/auth/index.html"
+  },
+  {
+    "revision": "e62f0e3456a75337beab",
+    "url": "/auth/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "f72d073b1ec83c10853d",
+    "url": "/auth/static/css/main.a4a7a78a.chunk.css"
+  },
+  {
+    "revision": "e62f0e3456a75337beab",
+    "url": "/auth/static/js/2.69a7a156.chunk.js"
+  },
+  {
+    "revision": "f72d073b1ec83c10853d",
+    "url": "/auth/static/js/main.71f62313.chunk.js"
+  },
+  {
+    "revision": "d9022639686edba99362",
+    "url": "/auth/static/js/runtime~main.7372c815.js"
+  },
+  {
+    "revision": "7d6e8a02913c378c3d59fa9208910bfe",
+    "url": "/auth/static/media/balmain.7d6e8a02.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/auth/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "c9b95f45e9b2f28c5b23ee95792b55d3",
+    "url": "/auth/static/media/bg13.c9b95f45.jpg"
+  },
+  {
+    "revision": "6cdd0e889a988dcdeeea445ac8ad13ea",
+    "url": "/auth/static/media/bg14.6cdd0e88.jpg"
+  },
+  {
+    "revision": "1d5476213e47699664a1641b17ba69e6",
+    "url": "/auth/static/media/bg15.1d547621.jpg"
+  },
+  {
+    "revision": "150b1c9bce62ec7a525a2e4d3ae34837",
+    "url": "/auth/static/media/bg16.150b1c9b.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/auth/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/auth/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/auth/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/auth/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ebe9884b2152a08a525fcd5054019532",
+    "url": "/auth/static/media/image_placeholder.ebe9884b.jpg"
+  },
+  {
+    "revision": "f803982222c87392f69787c2a3899de6",
+    "url": "/auth/static/media/james.f8039822.jpg"
+  },
+  {
+    "revision": "a80cd7a77206b15ee13a582ea53b4cab",
+    "url": "/auth/static/media/logo-white.a80cd7a7.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/auth/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/auth/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/auth/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/auth/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/auth/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f771455e07d39b0841a3f0d4a2143c5d",
+    "url": "/auth/static/media/placeholder.f771455e.jpg"
+  },
+  {
+    "revision": "d9f663d65c11bd0719aa01a3403d0062",
+    "url": "/auth/static/media/prada.d9f663d6.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/auth/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "4fa6245a9148ab4874183ab99fa818d4",
+    "url": "/auth/static/media/saint-laurent.4fa6245a.jpg"
+  }
+]);
